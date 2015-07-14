2826d3e8341d68233424c142f15ed99b54c33dee
Truman Zhou <truman.ck.zhou@gmail.com>
2015-07-14 22:01:01 +0800

    JPA CRUD

--- HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
+++ HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
@@ -17,8 +17,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.mapping.Table;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 import trumanz.HibernateLearn.EmployeeWithXmlMap;
 
@@ -30,7 +32,7 @@ public class EntityManagerTest {
 	public static class TestBean {
 		@Id
 	    @GeneratedValue(strategy = GenerationType.AUTO)
-	    private Long id;
+	    public Long id;
 
 	    private String firstName;
 
@@ -46,26 +48,49 @@ public class EntityManagerTest {
 	@BeforeClass
 	public static void setUpTable(){
 		Configuration conf = new Configuration().configure().addAnnotatedClass(TestBean.class);
-		
 		SessionFactory factory = conf.buildSessionFactory();
 		factory.close();
 	}
 	
 	
 	@Test
-	public void test() {
+	public void CRUD_TEST() {
+	
 	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testUnit");
 		
-
 		EntityManager em = emf.createEntityManager();
 		
-	
+		String name = "TestName";
+		
+		//Create
 		em.getTransaction().begin();
 		TestBean e = new TestBean();
+		e.setFirstName(name);
 		em.persist(e);
 		em.getTransaction().commit();
+		
+		//Retrieval
+		TestBean e2 = em.find(TestBean.class, e.id);
+		Assert.assertEquals(e2.firstName, e.firstName);
+		Assert.assertTrue(e.getFirstName().equals(e.getFirstName()));
 
+		String name2 = name + "append";
+		//Update
+		em.getTransaction().begin();
+		e2.setFirstName(name2);
+		em.getTransaction().commit();
+		TestBean e3 = em.find(TestBean.class, e.id);
+		Assert.assertEquals(e3.firstName, name2);
+		Assert.assertFalse(e3.getFirstName().equals(name));
+		
+	
+		//Delete
+		em.getTransaction().begin();
+		em.remove(e3);
+		em.getTransaction().commit();
+		TestBean e4 = em.find(TestBean.class, e.id);
+		Assert.assertEquals(e4, null);
 	}
 
 }
