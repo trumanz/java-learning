14b02c5a1163b712697702161a73f8d85bc027f0
Truman Zhou <truman.ck.zhou@gmail.com>
2015-07-14 14:54:46 +0800

    JPA

--- HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
+++ HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
@@ -1,19 +1,41 @@
 package trumanz.HibernateLearn.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
 import javax.persistence.Persistence;
 
+import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
+import trumanz.HibernateLearn.Employee;
 
 
 public class EntityManagerTest {
 
+	@Entity
+	static public class TestBean{
+		@Id
+		public int id;
+		public String name;
+	}
+	
 	@Test
 	public void test() {
+	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("testUnit");
-		
-		
+
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		TestBean e = new TestBean();
+		em.persist(e);
+		em.getTransaction().commit();
+
 	}
 
 }
