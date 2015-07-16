697c1e04a594c5be7cd8c5ecbefc6d18b2a550ea
Truman Zhou <truman.ck.zhou@gmail.com>
2015-07-16 10:33:57 +0800

    pointer to same object test

--- HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
+++ HibernateLearn/src/test/java/trumanz/HibernateLearn/jpa/EntityManagerTest.java
@@ -12,7 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Persistence;
 
-
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -45,8 +45,11 @@ public class EntityManagerTest {
 		}
 	}
 	
+	static Logger logger =  Logger.getLogger(EntityManagerTest.class);
+	
 	@BeforeClass
 	public static void setUpTable(){
+		//hiberante.cfg.xml  hibernate.hbm2ddl.auto configuration for update table in DB
 		Configuration conf = new Configuration().configure().addAnnotatedClass(TestBean.class);
 		SessionFactory factory = conf.buildSessionFactory();
 		factory.close();
@@ -65,32 +68,45 @@ public class EntityManagerTest {
 		
 		//Create
 		em.getTransaction().begin();
-		TestBean e = new TestBean();
-		e.setFirstName(name);
-		em.persist(e);
+		TestBean e1 = new TestBean();
+		e1.setFirstName(name);
+		em.persist(e1);
 		em.getTransaction().commit();
 		
+		logger.info("e1:" + e1.toString());
+		
 		//Retrieval
-		TestBean e2 = em.find(TestBean.class, e.id);
-		Assert.assertEquals(e2.firstName, e.firstName);
-		Assert.assertTrue(e.getFirstName().equals(e.getFirstName()));
+		TestBean e2 = em.find(TestBean.class, e1.id);
+		Assert.assertEquals(e2.firstName, e1.firstName);
+		Assert.assertTrue(e2.getFirstName().equals(e1.getFirstName()));
+		
+		logger.info("e2:" + e2.toString());
 
 		String name2 = name + "append";
 		//Update
 		em.getTransaction().begin();
 		e2.setFirstName(name2);
 		em.getTransaction().commit();
-		TestBean e3 = em.find(TestBean.class, e.id);
+		TestBean e3 = em.find(TestBean.class, e1.id);
 		Assert.assertEquals(e3.firstName, name2);
 		Assert.assertFalse(e3.getFirstName().equals(name));
 		
+		logger.info("e3:" + e3.toString());
+
 	
+		//they are all pointer to  same object
+		Assert.assertTrue(e1 == e2);
+		Assert.assertTrue(e2 == e3);
+		
 		//Delete
 		em.getTransaction().begin();
 		em.remove(e3);
 		em.getTransaction().commit();
-		TestBean e4 = em.find(TestBean.class, e.id);
+		TestBean e4 = em.find(TestBean.class, e1.id);
 		Assert.assertEquals(e4, null);
+		
+		
+		
 	}
 
 }
