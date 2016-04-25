e6a6dcca2de701e42b7b3250ad04618ecddd0c96
trumanz <Truman.Zhou@emc.com>
2016-04-25 18:33:22 +0800

    regex match

--- JavaLearnMisc/src/test/java/javalearn/misc/regex/Match.java
+++ JavaLearnMisc/src/test/java/javalearn/misc/regex/Match.java
@@ -1,5 +1,28 @@
 package javalearn.misc.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Match {
+
+	@Test
+	public void  regexGetData() {
+		CharSequence str1 = "Step-1/2, CreateVM-34/56";
+		
+		Pattern dataPatt = Pattern.compile("^Step-(\\d+)/(\\d+), (\\S+)-(\\d+)/(\\d+)$");
+		Matcher m = dataPatt.matcher(str1);
+		
+		Assert.assertTrue(m.matches());
+		Assert.assertEquals(5, m.groupCount());
+		Assert.assertEquals("1", m.group(1));
+		Assert.assertEquals("2", m.group(2));
+		Assert.assertEquals("CreateVM", m.group(3));
+		Assert.assertEquals("34", m.group(4));
+		Assert.assertEquals("56", m.group(5));
+		 
+ 
+	}
 
 }
